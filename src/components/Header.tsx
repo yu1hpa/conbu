28149dd0698d6d9b2c70b24b92bf6642d262aadf
yu1hpa <56914289+yu1hpa@users.noreply.github.com>
2022-01-16 00:52:11 +0900

    fix: Fix a bug that does not change the display (#14)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,25 +2,27 @@ import React from "react";
 
 export const Header = () => {
   return(
-    <div>
-      <header className="py-2 border-b-4 border-gray-300">
-        <div  className="py-4 grid place-items-center">
-          <a className="text-center hover:opacity-50">
-            <h1 className="text-5xl font-mono text-pink-800">
-              <a href="https://github.com/yu1hpa/conbu">CONBU</a>
-            </h1>
-          </a>
-        </div>
-        <div className="flex justify-center">
-          <span className="underline decoration-green-600 text-pink-800">CO</span>
-            VID-19 INFECTED PEOPLE SOI
-          <span className="underline decoration-green-600 text-pink-800">N</span>
-            SUU
-          <span className="underline decoration-green-600 text-pink-800">BU</span>
-            NKAI
-            (素因数分解)
-        </div>
-      </header>
-    </div>
+    <header className="py-2 border-b-4 border-gray-300">
+      <title>CONBU</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <div  className="py-4 grid place-items-center">
+        <a className="text-center hover:opacity-50">
+          <h1 className="text-5xl font-mono text-pink-800">
+            <a href="https://github.com/yu1hpa/conbu">CONBU</a>
+          </h1>
+        </a>
+      </div>
+      <div className="flex justify-center">
+        <span className="underline decoration-green-600 text-pink-800">CO</span>
+          VID-19 INFECTED PEOPLE SOI
+        <span className="underline decoration-green-600 text-pink-800">N</span>
+          SUU
+        <span className="underline decoration-green-600 text-pink-800">BU</span>
+          NKAI
+          (素因数分解)
+      </div>
+    </header>
   );
 };
