3e05f9f18373075a2f02315109369d3f7ddaea37
yu1hpa <yu.1hpa@gmail.com>
2022-01-16 00:34:52 +0900

    refactor: split to a header file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Patient from "./components/Patient";
-import { Footer } from "./components/Footer"
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-200 h-screen">
+      <Header />
       <main className="flex-grow">
         <Patient />
       </main>
